bd33d96822d25b948a3dcd0d369e92c670a28aa8
Ron Gidron <ron@rongidron.me>
2012-04-25 16:00:28 +0300

    Debugging on the service now platform.
    Lots of design bugs and API usage fixes
    getting getallapplications to work as an example.
    TODO - Find out the XPATH to use with gs.getXMLText

--- NolioAPI.js
+++ NolioAPI.js
@@ -8,19 +8,18 @@ var NolioAPI = Class.create();
 
 NolioAPI.prototype = {
 
+	this._url = null;    	  	// the soap end point url for the Nolio automation server the current instance is attached to
+	this._nacUser = null;  		// the Nolio Automation Server user name - used per each request
+	this._pwd = null;   		// the Nolio Automation Server password - used per each request
+	this._soapRequest = null;	// a "singleton" soap request object used by the current API instance
+	this._soapEnvelope = null;	// a "singleton" soap envelope object used by the current API instance
+	this.useECC = false;		// a flag to execute soap using the Service-now ECC queue or not
+
 	/*
 	 *   Each NolioAPI class instance works with a dedicated NAC and predefined credentials 
 	 *   Instances and credentials are set in the Nolio automation application tables (included in update set ..)
 	 */	
 	initialize: function(url, username, password){
-	
-		this._url = null;    	  	// the soap end point url for the Nolio automation server the current instance is attached to
-		this._nacUser = null;  		// the Nolio Automation Server user name - used per each request
-		this._pwd = null;   		// the Nolio Automation Server password - used per each request
-		this._soapRequest = null;	// a "singleton" soap request object used by the current API instance
-		this._soapEnvelope = null;	// a "singleton" soap envelope object used by the current API instance
-		this.useECC = false;		// a flag to execute soap using the Service-now ECC queue or not
-	
 	
 		if(typeof url != "undefined" && url != null){
 			this._url = url;
@@ -47,7 +46,85 @@ NolioAPI.prototype = {
 	   	this._soapEnvelope.createNameSpace("xmlns:mod", "http://model.api.dataservices.server.platform.nolio.com/");	
 	},
 	
+/*
+ *==============================================================================================================================*
+ *==================================	Utility methods used by the API functions	============================================*
+ *==============================================================================================================================*
+ */	
+	// returns a new SOAPEnvelope for fresh use 
+	getNewEnvelope: function(){
+		// Initialize the SOAPEnvelope and set the names spaces so we don't have to repeate this in every API call
+		_soapEnvelope = new SOAPEnvelope();
+   		_soapEnvelope.createNameSpace("xmlns:soap", "http://www.w3.org/2003/05/soap-envelope");
+	   	_soapEnvelope.createNameSpace("xmlns:mod", "http://model.api.dataservices.server.platform.nolio.com/");	
+	   	return _soapEnvelope;	
+	},
 	
+	// executes soap requests either directly or through the service-now ECC queue - returns XML to enable XPATH for the API
+	executeSOAP: function(envelope){
+		if(typeof envelope != "undefined" && envelope != null) {
+			if(useECC==false){
+				_soapRequest.post(this._soapEnvelope);
+				//return new XMLDocument(_soapRequest.getResponseDoc());
+			}else{
+				_soapRequest.post(this._soapEnvelope, true);
+				//return new XMLDocument(_soapRequest.getResponseDoc());
+			}
+			var status = request.getHttpStatus();
+			if (status != 200) {
+  				gs.log("NolioAPI - ERROR: soap error " + status);
+  				return null;
+			}
+			return new XMLDocument(_soapRequest.getResponseDoc());
+		}
+		gs.log("NolioAPI - ERROR: call to executeSOAP with null or undefined envelope");
+		return null;
+	},
+
+	/*
+ *==============================================================================================================================*
+ *==================================	Getters & Setters   	================================================================*
+ *==============================================================================================================================*
+ */		
+	getNACuser: function(){
+		return _nacUser;
+	},
+	
+	setNACuser: function(username){
+		if(typeof _nacUser != "undefined" && _nacUser != null){
+			this._nacUser = username;
+		}
+		return;
+	},
+	
+	getNACUrl: function() {
+		return _url;
+	},
+	
+	setNACUrl: function(url){
+		if(typeof url != "undefined" && url != null){
+			this._url = url;
+		}
+		return;
+	},
+
+	setNACPassword: function(pwd){
+		//TODO: understand how to properly use passwords here
+		this._pwd = pwd;
+	},
+
+	getUseECC: function(){
+		return useECC;
+	},
+
+	setUseECC: function(flag){
+		if(flag!="false" && flag!="true"){
+			gs.log("NolioAPI - Warninng: Trying to set useECC to a non boolean value, ignoring.");
+			return;
+		}
+		useECC = flag;
+	},
+
 	/*
 	 *  Returns a list of application names available for the current user (_nacUser) from the current server (_url)   
 	 */
@@ -97,86 +174,5 @@ NolioAPI.prototype = {
 		_soapEnvelope.createElement("mod:getAllAgents", "mod:password", _pwd);
 		var xml = executeSOAP(_soapEnvelope);
 		return xml.getNodeText("//ns2:hostName");	
-	},
-
-
-
-/*
- *==============================================================================================================================*
- *==================================	Utility methods used by the API functions	============================================*
- *==============================================================================================================================*
- */	
-	// returns a new SOAPEnvelope for fresh use 
-	getNewEnvelope: function(){
-		// Initialize the SOAPEnvelope and set the names spaces so we don't have to repeate this in every API call
-		_soapEnvelope = new SOAPEnvelope();
-   		_soapEnvelope.createNameSpace("xmlns:soap", "http://www.w3.org/2003/05/soap-envelope");
-	   	_soapEnvelope.createNameSpace("xmlns:mod", "http://model.api.dataservices.server.platform.nolio.com/");	
-	   	return _soapEnvelope;	
-	},
-	
-	// executes soap requests either directly or through the service-now ECC queue - returns XML to enable XPATH for the API
-	executeSOAP: function(envelope){
-		if(typeof envelope != "undefined" && envelope != null) {
-			if(useECC==false){
-				_soapRequest.post(this._soapEnvelope);
-				//return new XMLDocument(_soapRequest.getResponseDoc());
-			}else{
-				_soapRequest.post(this._soapEnvelope, true);
-				//return new XMLDocument(_soapRequest.getResponseDoc());
-			}
-			var status = request.getHttpStatus();
-			if (status != 200) {
-  				gs.log("NolioAPI - ERROR: soap error " + status);
-  				return null;
-			}
-			return new XMLDocument(_soapRequest.getResponseDoc());
-		}
-		gs.log("NolioAPI - ERROR: call to executeSOAP with null or undefined envelope");
-		return null;
-	},
-	
-/*
- *==============================================================================================================================*
- *==================================	Getters & Setters   	================================================================*
- *==============================================================================================================================*
- */		
-	getNACuser: function(){
-		return _nacUser;
-	},
-	
-	setNACuser: function(username){
-		if(typeof _nacUser != "undefined" && _nacUser != null){
-			this._nacUser = username;
-		}
-		return;
-	},
-	
-	getNACUrl: function() {
-		return _url;
-	},
-	
-	setNACUrl: function(url){
-		if(typeof url != "undefined" && url != null){
-			this._url = url;
-		}
-		return;
-	},
-
-	setNACPassword: function(pwd){
-		//TODO: understand how to properly use passwords here
-		this._pwd = pwd;
-	},
-
-	getUseECC: function(){
-		return useECC;
-	},
-
-	setUseECC: function(flag){
-		if(flag!="false" && flag!="true"){
-			gs.log("NolioAPI - Warninng: Trying to set useECC to a non boolean value, ignoring.");
-			return;
-		}
-		useECC = flag;
 	}
 };
